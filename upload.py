98859cede71f59747af96d59d24a5ddb7413e296
toddn <tcnichol@illinois.edu>
2020-10-03 14:11:27 -0500

    use client instead

--- upload.py
+++ upload.py
@@ -5,10 +5,8 @@ import pyclowder.files
 def main():
     url = 'https://clowder.ncsa.illinois.edu/clowder'
     userkey = sys.argv[1]
-    path_to_file = sys.argv[2]
-    dataset_id = sys.argv[3]
-    file_id = pyclowder.files.upload_to_dataset(None, url, userkey, dataset_id=dataset_id,filepath=path_to_file)
-    print('uploaded file', file_id)
+    client = pyclowder.files.ClowderClient(host=url, key=userkey)
+    client
 
 
 if __name__ == '__main__':
