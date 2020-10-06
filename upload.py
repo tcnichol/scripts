f703680f81658ac173ac8803761309bf3422f78c
toddn <tcnichol@illinois.edu>
2020-10-06 09:51:17 -0500

    uploads a file to a dataset

--- upload.py
+++ upload.py
@@ -1,12 +1,17 @@
 import pyclowder
 import sys
 import pyclowder.files
+import pyclowder.datasets
 
 def main():
     url = 'https://clowder.ncsa.illinois.edu/clowder'
     userkey = sys.argv[1]
-    client = pyclowder.files.ClowderClient(host=url, key=userkey)
-    client
+    path_to_file = sys.argv[2]
+    dataset_id = sys.argv[3]
+    print(userkey, path_to_file, dataset_id)
+    client = pyclowder.datasets.ClowderClient(host=url, key=userkey)
+    file_id = client.post_file("/uploadToDataset/%s" % dataset_id, path_to_file)
+    print("uploaded file", file_id)
 
 
 if __name__ == '__main__':
