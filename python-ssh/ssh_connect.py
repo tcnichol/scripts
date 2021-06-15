e578ab1c44df25c4ac4ebaedc3974bc6e82ca86d
toddn <tcnichol@illinois.edu>
2021-06-15 13:00:05 -0500

    adding method for uploading a file

--- python-ssh/ssh_connect.py
+++ python-ssh/ssh_connect.py
@@ -1,5 +1,6 @@
 import paramiko
 from paramiko import SSHClient
+from scp import SCPClient
 import sys
 hostname = sys.argv[1]
 password = sys.argv[2]
@@ -26,6 +27,21 @@ def execute_commands(client, commands: [str]):
             print(f"INPUT: {cmd} | OUTPUT: {line}")
     return responses
 
+# Define progress callback that prints the current percentage completed for the file
+def progress(filename, size, sent):
+    sys.stdout.write("%s's progress: %.2f%%   \r" % (filename, float(sent)/float(size)*100) )
+
+
+def upload_to_remote(client: SSHClient, path_to_file, destination):
+    ssh_client = paramiko.SSHClient()
+    ssh_client.load_system_host_keys()
+
+    ssh_client.connect(hostname, username=username,password=password)
+
+    scp = SCPClient(client.get_transport())
+    scp.put(path_to_file, recursive=True,remote_path=destination)
+    scp.close()
+
 def main():
     print(sys.argv)
     ssh_client = paramiko.SSHClient()
@@ -35,10 +51,17 @@ def main():
 
     stdin, stdout, stderr = ssh_client.exec_command('ls \n cd ocean \n ls')
     print(stdout.read().decode("utf8"))
+    result = execute_commands(client=ssh_client, commands=['ls', 'cd ocean', 'ls'])
     stdin, stdout, stderr = ssh_client.exec_command('cd ocean \n ls')
     print(stdout.read().decode("utf8"))
     ssh_client.close()
     print('done')
 
 if __name__ == '__main__':
+    ssh_client = paramiko.SSHClient()
+    ssh_client.load_system_host_keys()
+
+    ssh_client.connect(hostname, username=username,password=password)
+
+    upload_to_remote(client=ssh_client, path_to_file='/Users/helium/ncsa/scripts/python-ssh/test.txt', destination='/jet/home/pdg/ocean')
     main()
