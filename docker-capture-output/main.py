df7767f4063193b4d623096d3e4c030fc766672f
toddn <tcnichol@illinois.edu>
2021-06-16 13:18:34 -0500

    docker shows output with print

---
+++ docker-capture-output/main.py
@@ -0,0 +1,13 @@
+import time
+import sys
+import logging
+
+def main():
+    for i in range(0,100):
+        print(i)
+        time.sleep(5)
+    print('done')
+
+
+if __name__ == "__main__":
+    main()
