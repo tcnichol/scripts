89d37e4e7663822a3ba03e29d5c6017042474697
toddn <tcnichol@illinois.edu>
2021-06-17 14:00:03 -0500

    examples for using gdal

---
+++ gdal-docker/using-conda/main.py
@@ -0,0 +1,11 @@
+from osgeo import gdal
+import tensorflow as tf
+import matplotlib
+import matplotlib.pyplot
+import shapefile
+
+def main():
+    print('did the imports work')
+
+if __name__ == "__main__":
+    main()
---
+++ gdal-docker/using-pip/main.py
@@ -0,0 +1,8 @@
+import gdal
+
+
+def main():
+    print('did the imports work')
+
+if __name__ == "__main__":
+    main()
